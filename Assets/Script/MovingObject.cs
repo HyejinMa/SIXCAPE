46dc2cf24703466dfb4a1ff9e0ad204ce0733f1b
HyejinMa <mahj345@gmail.com>
2020-08-19 19:37:24 +0900

    콜라이더 충돌 감지 오류 해결
    오브젝트에 적용한 스크립트에 layer을 설정했는데,
    전봉지의 layer과 예시 오브젝트의 layer이 같지 않아 생긴 오류였음.

--- Assets/Script/MovingObject.cs
+++ Assets/Script/MovingObject.cs
@@ -51,7 +51,7 @@ public class MovingObject : MonoBehaviour
         RaycastHit2D hit; //A지점에서 B지점에 레이저 쏠때 방해물이 없으면 hit에 null값 리턴, 있으면 방해물 리턴
 
         Vector2 start = transform.position; // A지점, 캐릭터의 현재 위치 값
-        Vector2 end = start + new Vector2(vector.x * speed, vector.y * speed); //B지점, 캐릭터가 이동하고자 하는 위치 값
+        Vector2 end = start + new Vector2(vector.x * speed * walkCount, vector.y * speed * walkCount); //B지점, 캐릭터가 이동하고자 하는 위치 값
 
         boxCollider.enabled = false; // 캐릭터가 가만히 있을 경우 자기 스스로 콜라이더에 충돌하기 때문에 잠깐 꺼주어야한다.
         hit = Physics2D.Linecast(start, end, layerMask);
@@ -70,12 +70,13 @@ public class MovingObject : MonoBehaviour
                 {
                     transform.Translate(0, vector.y * (speed + applyRunSpeed), 0);
                 }
+                if (applyRunFlag)
+                    currentWalkCount++;
 
                 currentWalkCount++;
                 yield return new WaitForSeconds(0.01f);
 
             }
-
             currentWalkCount = 0;
             canMove = true;
         }// 원래 이동되는 픽셀 즉 speed와 walkCount를 곱했을때 나오는 숫자가 크면 순간이동 하는 것처럼 보이게됨 이를 방지하기 위해 대기 코루틴 작성
