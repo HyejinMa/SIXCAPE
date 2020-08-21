d17236be2c6cfa89351674ed402fe5abdec1703d
DESKTOP-BFN2B16\USER <na31766@gmail.com>
2020-08-21 20:36:57 +0900

    ESC 누르면 옵션

---
+++ Assets/UI/Option.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Option : MonoBehaviour
+{
+    public GameObject MenuSet;
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+            MenuSet.SetActive(true);
+    }
+}
