1b2103239338c42cf3119eef3af813624ea3328b
DESKTOP-BFN2B16\USER <na31766@gmail.com>
2020-08-22 15:59:46 +0900

    옵션, start 씬 UI 구현

---
+++ Assets/Scenes/bExit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bExit : MonoBehaviour{
+
+    public void OnclickExit()
+    {
+
+        Application.Quit();
+        Debug.Log("Button Click");
+
+    }
+}
--- Assets/UI/Option.cs
+++ Assets/UI/Option.cs
@@ -1,13 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Option : MonoBehaviour
 {
     public GameObject MenuSet;
+    public GameObject player;
     void Update()
     {
         if (Input.GetButtonDown("Cancel"))
             MenuSet.SetActive(true);
     }
+    public void OptionConfirm() // 옵션 종료하는 창
+    {
+        MenuSet.SetActive(false);
+        Time.timeScale = 1;
+    }
+    public void GameSave()
+    {
+        PlayerPrefs.SetFloat("PlayerX", player.transform.position.x);
+        PlayerPrefs.SetFloat("PlayerY", player.transform.position.y);
+        PlayerPrefs.Save();
+        //player.x, player.y
+        MenuSet.SetActive(false);
+    }
+
+    public void GameLoad()
+    {
+        float x = PlayerPrefs.GetFloat("playerX");
+        float y = PlayerPrefs.GetFloat("PlayerY");
+
+        player.transform.position = new Vector3(x, y, 0);
+
+    }   
+    
+    public void ExitMap() // 스테이즈 나가기
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Start");
+    }
+    
 }
